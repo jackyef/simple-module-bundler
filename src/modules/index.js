c0649d9615459f6f473d8c08654767e2aaabbf6a
Jacky Efendi <jacky.efendi1@gmail.com>
2019-07-09 15:46:03 +0700

    feat: Initial commit

---
+++ src/modules/index.js
@@ -0,0 +1,4 @@
+import someString from './someString';
+import generateRandomNumber from './generateRandomNumber';
+
+console.log(`Hello, ${someString}, your lucky number is: ${generateRandomNumber()}`);
